2a2de80fe4ec9f71af06edd1576c45e74f37de8f
Naman <namantiwari.nt@gmail.com>
2019-03-10 20:21:31 +0530

    Homework 3 - Implement localMaxima

--- Homework3/Homework3.hs
+++ Homework3/Homework3.hs
@@ -5,8 +5,15 @@ import Data.List
 -- zipWith func list1 list2
 -- replicate (length list) list
 -- filter(\x -> x mod n == 0) []
-skip:: Int -> [a] -> [a]
+skip :: Int -> [a] -> [a]
 skip n = map snd . filter (\x -> mod (fst x) n == 0) . zip [1..]
 
-skips:: [a] -> [[a]]
+skips :: [a] -> [[a]]
 skips xs = zipWith ($) (map skip [1..]) $ replicate (length xs) xs
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima (a : b : c : xs)
+  | (b > c) && (b > a) = b : localMaxima (b : c : xs)
+  | otherwise = localMaxima (b : c : xs)
+
+localMaxima _ = []
