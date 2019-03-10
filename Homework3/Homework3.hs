0444618ac8c8a6cccbc5c87d28828b14b65f6fb2
Naman <namantiwari.nt@gmail.com>
2019-03-10 20:06:21 +0530

    Homework 3 - Implement skips

--- Homework3/Homework3.hs
+++ Homework3/Homework3.hs
@@ -1,1 +1,12 @@
 module Homework3.Homework3 where
+
+import Data.List
+
+-- zipWith func list1 list2
+-- replicate (length list) list
+-- filter(\x -> x mod n == 0) []
+skip:: Int -> [a] -> [a]
+skip n = map snd . filter (\x -> mod (fst x) n == 0) . zip [1..]
+
+skips:: [a] -> [[a]]
+skips xs = zipWith ($) (map skip [1..]) $ replicate (length xs) xs
